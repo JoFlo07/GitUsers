e41ef5659acb1b85a359e938314e8bd390963154
Florian Jost <florian.jost89@gmail.com>
2019-09-23 16:48:44 +0200

    chore: folder structure

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {
-  Text, View,
+  Text, View, StyleSheet,
 } from 'react-native';
 
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>Hi</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;

bd4f0ebb980148c8c2b1ee68c3517bc6ebfdf557
Florian Jost <florian.jost89@gmail.com>
2019-09-24 10:49:54 +0200

    feat: add reducer function

--- redux/actions.ts
+++ redux/actions.ts
@@ -2,7 +2,7 @@ export const FETCH_USERS_PENDING = 'FETCH_USERS_PENDING';
 export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
 
-function fetchUsersPending () {
+function fetchUsersPending() {
   return {
     type: FETCH_USERS_PENDING,
   };
